6111d5caa7c2e455b08391678b26be93e14356c3
Anton V. Novoseltsev <nova177dev@gmail.com>
2026-01-05 14:17:57 +0400

    Update unimap-handler.js
    Updated API base URL and Mapbox access token to empty strings.

--- SlimVectorTileServer/wwwroot/js/unimap-handler.js
+++ SlimVectorTileServer/wwwroot/js/unimap-handler.js
@@ -1,6 +1,6 @@
 // Configuration
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:5035/api',
+    API_BASE_URL: '',
     MAP_SETTINGS: {
         initialCenter: [-74.6071028, 40.6931568],
         initialZoom: 0,
@@ -15,7 +15,7 @@ const CONFIG = {
 
 // Replace this with your own token or implement a more secure approach
 //mapboxgl.accessToken = '@Environment.GetEnvironmentVariable("MAPBOX_ACCESS_TOKEN")';
-mapboxgl.accessToken = 'pk.eyJ1Ijoibm92YTE3N3J1cyIsImEiOiJja3oyc2Q4Y3UwMTVuMnZwMjFiOWl2eHo1In0.cpXR0UPWNtpLKonGRe5hpA';
+mapboxgl.accessToken = '';
 
 // Initialize map
 const map = new mapboxgl.Map({
